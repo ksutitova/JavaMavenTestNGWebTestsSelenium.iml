0d5342bcd49c8f7acc8ae71891677e269c2363a6
ktitova <ksunchik123@yandex.ru>
2022-05-14 20:35:46 +0300

    WebTest

---
+++ src/main/java/WebTest.java
@@ -0,0 +1,46 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WebTest {
+
+    @Test
+    public void testMenuStartTitle() throws InterruptedException {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "/Users/ksenianehotina/Downloads/chromedriver 2";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Welcome to 99 Bottles of Beer";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement menuBrowseLanguages = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href=\"/abc.html\"]")
+        );
+
+        menuBrowseLanguages.click();
+
+        WebElement menuStart = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
+        );
+
+        menuStart.click();
+
+        WebElement h2 = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='main']/h2")
+        );
+
+        String actualResult = h2.getText();
+
+        Assert.assertEquals(actualResult,expectedResult);
+
+        driver.quit();
+    }
+}
